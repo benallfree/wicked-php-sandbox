d2b872abff96e39856b14671248aacea7eced7ee
Ben Allfree <ben@benallfree.com>
2012-08-17 07:38:45 -0700

    Added eval_php() listener

--- load.php
+++ load.php
@@ -1,3 +1,10 @@
 <?
 
 W::add_mixin('EvalPhpMixin');
+
+W::register_filter('eval_php', function($php, $vars) {
+  $fname = tempnam(sys_get_temp_dir(), "wicked_eval_php");
+  file_put_contents($fname, $php);
+  $s = W::php_sandbox($fname, $vars, true);
+  return $s;
+});
